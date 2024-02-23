52020f50dac8636c60d9e90d7dbc2c24127abd5a
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2024-02-23 19:00:01 +0100

    A little bit more house cleaning

--- packages/nextjs/components/index/testimonials.js
+++ packages/nextjs/components/index/testimonials.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import React from "react";
-import Container from "./container";
-
+import Image from "next/image";
 import userOneImg from "../../public/user1.jpg";
 import userTwoImg from "../../public/user2.jpg";
 import userThreeImg from "../../public/user3.jpg";
+import Container from "./container";
 
-const Testimonials  = () => {
+const Testimonials = () => {
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
@@ -33,8 +32,7 @@ const Testimonials  = () => {
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-base-300 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal text-inherit dark:text-inherit">
-              This is an <Mark>awesome</Mark> landing page template I&apos;ve seen. I
-              would use this for anything.
+              This is an <Mark>awesome</Mark> landing page template I&apos;ve seen. I would use this for anything.
             </p>
 
             <Avatar image={userThreeImg} name="Gabrielle Winn" title="Co-founder of Acme Inc" />
@@ -43,19 +41,13 @@ const Testimonials  = () => {
       </div>
     </Container>
   );
-}
+};
 
 function Avatar(props) {
   return (
     <div className="flex items-center mt-8 space-x-3">
       <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
-        <Image
-          src={props.image}
-          width="40"
-          height="40"
-          alt="Avatar"
-          placeholder="blur"
-        />
+        <Image src={props.image} width="40" height="40" alt="Avatar" placeholder="blur" />
       </div>
       <div>
         <div className="text-lg font-medium dark:text-inherit">{props.name}</div>
