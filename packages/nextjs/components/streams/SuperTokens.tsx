2546a0543b525233328e29b388b503e8e165ca26
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2024-03-02 22:49:51 +0100

    added FlowingBalance in Streams

--- packages/nextjs/components/streams/SuperTokens.tsx
+++ packages/nextjs/components/streams/SuperTokens.tsx
@@ -38,7 +38,7 @@ const SupertokensComponent: React.FC = () => {
     address: "0xcfA132E353cB4E398080B9700609bb008eceB125",
     abi: CFAv1ForwarderABI,
     functionName: "setFlowrate",
-    args: ["0x36d9a149895d905D117C38F3090f4344B76Ec9F4", receiverAddress, BigInt(100)],
+    args: ["0x36d9a149895d905D117C38F3090f4344B76Ec9F4", receiverAddress, BigInt(100000000000000)],
   });
 
   const { write: deleteFlow } = useContractWrite({
@@ -50,70 +50,76 @@ const SupertokensComponent: React.FC = () => {
 
   return (
     <>
-      <div>
-        <h1>Approve Upgrade</h1>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-          value={approveAmount}
-          onChange={e => setApproveAmount(e.target.value)}
-        />
-        <button className="btn btn-success" onClick={() => approveUpgrade()}>
-          Approve Upgrade
-        </button>
-      </div>
-      <div>
-        <h1>Upgrade</h1>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-          value={upgradeAmount}
-          onChange={e => setUpgradeAmount(e.target.value)}
-        />
-        <button className="btn btn-success" onClick={() => upgrade()}>
-          Upgrade
-        </button>
-      </div>
-      <div>
-        <h1>Downgrade</h1>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-          value={downgradeAmount}
-          onChange={e => setDowngradeAmount(e.target.value)}
-        />
-        <button className="btn btn-error" onClick={() => downgrade()}>
-          Downgrade
-        </button>
-      </div>
-      <div>
-        <h1>Set Flowrate</h1>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-          value={receiverAddress}
-          onChange={e => setReceiverAddress(e.target.value)}
-        />
-        <button className="btn btn-success" onClick={() => forwarder()}>
-          Set Flowrate
-        </button>
-      </div>
-      <div>
-        <h1>Delete Flowrate</h1>
-        <input
-          type="text"
-          placeholder="Type receiver address"
-          className="input input-bordered w-full max-w-xs"
-          value={deleteAddress}
-          onChange={e => setDeleteAddress(e.target.value)}
-        />
-        <button className="btn btn-error" onClick={() => deleteFlow()}>
-          Delete Flowrate
-        </button>
+      <div className="flex items-center justify-center space-y-8">
+        <div className="text-center">
+          <h1>Approve Upgrade</h1>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={approveAmount}
+            onChange={e => setApproveAmount(e.target.value)}
+          />
+          <button className="btn btn-success mt-2" onClick={() => approveUpgrade()}>
+            Approve Upgrade
+          </button>
+        </div>
+
+        <div className="text-center">
+          <h1>Upgrade</h1>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={upgradeAmount}
+            onChange={e => setUpgradeAmount(e.target.value)}
+          />
+          <button className="btn btn-success mt-2" onClick={() => upgrade()}>
+            Upgrade
+          </button>
+        </div>
+
+        <div className="text-center">
+          <h1>Downgrade</h1>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={downgradeAmount}
+            onChange={e => setDowngradeAmount(e.target.value)}
+          />
+          <button className="btn btn-error mt-2" onClick={() => downgrade()}>
+            Downgrade
+          </button>
+        </div>
+
+        <div className="text-center">
+          <h1>Set Flowrate</h1>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={receiverAddress}
+            onChange={e => setReceiverAddress(e.target.value)}
+          />
+          <button className="btn btn-success mt-2" onClick={() => forwarder()}>
+            Set Flowrate
+          </button>
+        </div>
+
+        <div className="text-center">
+          <h1>Delete Flowrate</h1>
+          <input
+            type="text"
+            placeholder="Type receiver address"
+            className="input input-bordered w-full max-w-xs"
+            value={deleteAddress}
+            onChange={e => setDeleteAddress(e.target.value)}
+          />
+          <button className="btn btn-error mt-2" onClick={() => deleteFlow()}>
+            Delete Flowrate
+          </button>
+        </div>
       </div>
     </>
   );
--- packages/nextjs/pages/streams.tsx
+++ packages/nextjs/pages/streams.tsx
@@ -1,12 +1,47 @@
 import { MetaHeader } from "../components/MetaHeader";
 import type { NextPage } from "next";
+import { useContractRead } from "wagmi";
+import { useAccount } from "wagmi";
+import FlowingBalance from "~~/components/streams/FlowingBalance";
 import SuperTokens from "~~/components/streams/SuperTokens";
+import { CFAv1ForwarderABI } from "~~/components/streams/abis/CFAv1Forwarder";
+import { SuperTokenABI } from "~~/components/streams/abis/SuperTokenABI";
 
 const Streams: NextPage = () => {
+  const account = useAccount();
+
+  const { data: balanceOf } = useContractRead({
+    address: "0x36d9a149895d905D117C38F3090f4344B76Ec9F4",
+    abi: SuperTokenABI,
+    functionName: "balanceOf",
+    args: [account.address],
+  });
+
+  console.log(balanceOf);
+
+  const { data: getAccountFlowrate } = useContractRead({
+    address: "0xcfA132E353cB4E398080B9700609bb008eceB125",
+    abi: CFAv1ForwarderABI,
+    functionName: "getAccountFlowrate",
+    args: ["0x36d9a149895d905D117C38F3090f4344B76Ec9F4", account.address],
+  });
+
   return (
     <>
       <MetaHeader title="XocDapp | Wagmi" description="XocDapp page" />
-      <SuperTokens />
+      <div className="flex flex-col m-10 p-20 items-center ">
+        <SuperTokens />
+        <div className="card w-3/5 bg-base-100 shadow-xl mt-16">
+          <div className="card-body">
+            <FlowingBalance
+              startingBalance={BigInt(balanceOf?.toString() ?? "0")}
+              startingBalanceDate={new Date()}
+              flowRate={BigInt(getAccountFlowrate?.toString() ?? "0")}
+            />
+            <div className="stat-title">Account&apos;s Flowrate</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
